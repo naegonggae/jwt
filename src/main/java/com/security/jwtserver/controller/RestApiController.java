68c5830cc6c283337d14bf1353f65b96aaaab5ca
naegonggae <tkdtkd97@naver.com>
2023-07-18 22:36:22 +0900

    :sparkles: jwt를 위한 yml파일 세팅

--- src/main/java/com/security/jwtserver/controller/RestApiController.java
+++ src/main/java/com/security/jwtserver/controller/RestApiController.java
@@ -1,5 +1,14 @@
 package com.security.jwtserver.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RestApiController {
+
+	@GetMapping("/home")
+	public String home() {
+		return "<h1>home</h1>";
+	}
 
 }
